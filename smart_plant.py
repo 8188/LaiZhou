63e0315e53db5c17ad2454fef5cbb9b68e3f8167
8188 <posuiredstar@163.com>
2022-08-22 21:32:07 +0800

    WSGI

--- smart_plant.py
+++ smart_plant.py
@@ -1,8 +1,11 @@
 from app import create_app
-from gevent import pywsgi
+from gevent import pywsgi, monkey
+from config import Config
 
 app = create_app()
-    
+ 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0')
-    pywsgi.WSGIServer(('0.0.0.0', 8990), app).serve_forever()
+    monkey.patch_all()
+    http_server = pywsgi.WSGIServer((Config.WSGI_HOST, Config.WSGI_PORT), app)
+    http_server.serve_forever()
